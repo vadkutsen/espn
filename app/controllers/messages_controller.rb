43dc1231fd012c9192a59302babe6839b7cdcc05
vadkutsen <vadkutsen@gmail.com>
2018-07-02 03:45:52 +0200

    Add buttons

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,35 @@
 class MessagesController < ApplicationController
-	
+	before_action :find_message, only: [:show, :edit, :update, :destroy]
+
+
 	def index
-		
+		@messages = Message.all.order("created_at DESC")
+	end
+
+	def new
+		@message = Message.new
+	end
+
+	def create
+		@message = Message.new(message_params)
+
+		if @message.save
+			redirect_to root_path
+		else
+			render 'new'
+		end
+	end
+
+	def show
+	end
+
+	private
+
+	def message_params
+		params.require(:message).permit(:title, :description)
+	end
+
+	def find_message
+		@message = Message.find(params[:id])
 	end
 end
